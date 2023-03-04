b2ca585f6e4c33bbaf6e07eefc4e96135205d79c
Dmitro-Kovalchuk <katerhem@gmail.com>
2023-03-04 12:15:51 +0200

    console.log +

--- js/JS-treining.js
+++ js/JS-treining.js
@@ -1,3 +1,15 @@
-console.log(1 + "2"); // "12"
-console.log(1 + "2" + 4); // "124"
-console.log(1 + 2 + "4"); // "34"
+// console.log(1 + "2"); // "12"
+// console.log(1 + "2" + 4); // "124"
+// console.log(1 + 2 + "4"); // "34"
+
+// function calculateTotalPrice (orderedQuantity, pricePerItem) {
+//   // Change code below this line
+//   // const totalPrice = (orderedQuantity * pricePerItem);
+//   const orderedQuantity = 5;
+//   const pricePerItem = 100;
+//   console.log(orderedQuantity * pricePerItem);
+//   // Change code above this line
+//   return totalPrice;
+// };
+
+
